69c16be663f891bf81e2b8904a066dae19efdaa2
Alex-Mak-MCW <77552818+Alex-Mak-MCW@users.noreply.github.com>
2025-07-31 20:18:22 -0600

    test fixes

--- Code/Model_Deployment/prototype.py
+++ Code/Model_Deployment/prototype.py
@@ -34,7 +34,7 @@ import streamlit.components.v1 as components
 
 import base64
 
-st.write("👉 scikit-learn version:", sckikit-learn.__version__)
+st.write("👉 scikit-learn version:", scikit-learn.__version__)
 
 # -----------------------------------------------
 
